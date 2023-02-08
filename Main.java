bdb8c3bf686f0eb58d19f013c141ef131e26f4fc
kinyhaha <kinyhaha@gmail.com>
2023-02-08 17:22:42 +0300

    featureMajor : add feature 2

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello");
 		System.out.println("Develop 1");
 		System.out.println("add major feature 1");
+		System.out.println("add major feature 2");
     }
 }
