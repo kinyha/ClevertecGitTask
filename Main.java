72e5d31beec78d1c40a1f0bebefad1229793cd80
kinyhaha <kinyhaha@gmail.com>
2023-02-08 17:57:14 +0300

    develop : develop 5

--- Main.java
+++ Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
 		System.out.println("Develop 1");
-
 		System.out.println("Develop 2");
 		System.out.println("Develop 3");
 		System.out.println("Develop 4");
+		System.out.println("Develop 5");
+		
 		System.out.println("add major feature 1");
 		System.out.println("add major feature 2");
 		System.out.println("add major feature 3");
 		System.out.println("add major feature 4");
 		System.out.println("add major feature 5");
-
     }
 }
